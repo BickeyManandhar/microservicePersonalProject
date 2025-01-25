5f4932eeda92318689441b71971381130032fd06
BickeyManandhar <manandharbickey@gmail.com>
2025-01-25 11:30:59 -0500

    using Environment interface to fetch application properties

--- accounts/src/main/java/com/personal/project/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/personal/project/accounts/controller/AccountController.java
@@ -13,8 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -34,11 +35,14 @@ public class AccountController {
     private final IAccountService accountService;
 
     @Value("${build.version}")
-    private String buildVersion;
+    private String buildVersion; //first approach to fetch application properties
 
     public AccountController(IAccountService accountService) {
         this.accountService = accountService;
     }
+
+    @Autowired
+    private Environment environment; //second approach to fetch application properties
 
     @Operation(
             summary = "Create Account REST API",
@@ -196,6 +200,30 @@ public class AccountController {
         return  ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get java version",
+            description = "REST API to fetch java version."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK."
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error.",
+                    content= @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+    @GetMapping("java-version")
+    public ResponseEntity<String> getJavaVersion(){
+        return  ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
     }
 
 }
